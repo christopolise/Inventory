6aa5a8079fb474588ed2ed14efcc4a7d2a6564a5
Unknown <chkitras@gmail.com>
2018-07-03 15:54:22 -0600

    Py_env
    - Includes the virtual environment used
    - added the hostname function

---
+++ invent_py/Inventory.py
@@ -0,0 +1,194 @@
+import subprocess
+import os
+
+
+def sudocheck():
+    """
+    Checks to see if user has sudo privileges
+    :return: void
+    """
+
+    access = os.getuid()
+    if access != 0:
+        print('You must run this script as root or under sudo')
+        exit(code=1)
+
+
+def prereqcheck():
+    """
+    Checks for and installs prerequisites if necessary
+    :return:
+    """
+    prerequisites = ['dmidecode', 'util-linux', 'ethtool', 'usbutils', 'gptfdisk', 'bc', 'emacs']
+    for i in range(len(prerequisites)):
+        rpm = subprocess.Popen(('rpm', '-qa', '--last'), stdout=subprocess.PIPE)
+        try:
+            output = subprocess.check_output(('grep', '-iw', prerequisites[i]), stdin=rpm.stdout)
+        except Exception as e:
+            output = None
+        rpm.wait()
+        if output is None:
+            print('Installing dependency: ' + prerequisites[i])
+            # subprocess.run(['zypper', '-n', 'install', prerequisites[i]])
+            os.system('zypper -n install ' + prerequisites[i])
+        else:
+            print('Dependency ' + prerequisites[i] + ' is already satisfied')
+
+
+def asset():
+    return 'ASSET'
+
+
+def hostname():
+    """
+    Determines the hostname of the machine via three methods
+    :return: Hostname of current machine
+    """
+    host_a = str(subprocess.check_output(('hostname', '-s'))).rstrip()
+    host_b1 = subprocess.Popen(('uname', '-n'), stdout=subprocess.PIPE)
+    host_b2 = subprocess.check_output(('sed', 's/[.].*$//'), stdin=host_b1.stdout)
+    host_b = str(host_b2).rstrip()
+    host_c1 = open('/etc/hostname')
+    host_c = host_c1.readline().rstrip()
+
+    if host_a is not None:
+        return host_a
+    elif host_b is not None:
+        return host_b
+    elif host_c is not None:
+        return host_c
+    else:
+        return "---"
+
+
+def pcode():
+    return 'PCODE'
+
+
+def mm():
+    return 'MM'
+
+
+def sdp():
+    return 'SDP'
+
+
+def serialnumber():
+    pass
+
+
+def makemodel():
+    pass
+
+
+def vendor():
+    pass
+
+
+def codename():
+    pass
+
+
+def cpuspeed():
+    pass
+
+
+def sockets():
+    pass
+
+
+def threads():
+    pass
+
+
+def hyperthreading():
+    pass
+
+
+def cpucores():
+    pass
+
+
+def ram():
+    pass
+
+
+def virttech():
+    pass
+
+
+def vtd():
+    return 'VTD'
+
+
+def hap():
+    pass
+
+
+def sriov():
+    return 'SRIOV'
+
+
+def numa():
+    pass
+
+
+def efi():
+    pass
+
+
+def pci():
+    return 'PCI'
+
+
+def pcix():
+    return 'PCIX'
+
+
+def pcie():
+    return 'PCIE'
+
+
+def usb3():
+    pass
+
+
+def networking():
+    pass
+
+
+def ssd():
+    pass
+
+
+def sata():
+    pass
+
+
+def space():
+    pass
+
+
+def boots():
+    return 'Y'
+
+
+def stable():
+    pass
+
+
+def cddvd():
+    pass
+
+
+def sra():
+    return 'SRA'
+
+
+def support():
+    pass
+
+
+sudocheck()
+# prereqcheck()
+hostname()
